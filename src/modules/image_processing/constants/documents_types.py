64a58fce4607fbf5f8e015b6fc013796653846d2
Mateus Aguiar <aguiar_m@ymail.com>
2024-02-11 20:26:51 -0300

    feat: enumerating possible documents types

---
+++ src/modules/image_processing/constants/documents_types.py
@@ -0,0 +1,4 @@
+IDENTIFICATION_DOCUMENT_FRONT = 'cnh_rg_frente'
+IDENTIFICATION_DOCUMENT_BACK = 'cnh_rg_tras'
+SELFIE_WITH_DOCUMENT = 'foto_com_documento'
+RESIDENCE_PROOF = 'comprovante_residencia'
