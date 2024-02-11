456c8f3a1effdfe1035dce476b98a7b81972888d
Mateus Aguiar <aguiar_m@ymail.com>
2024-02-11 20:27:44 -0300

    refactor: adding document type argument

--- src/modules/image_processing/interfaces/i_contextual_image_processor.py
+++ src/modules/image_processing/interfaces/i_contextual_image_processor.py
@@ -4,5 +4,5 @@ from src.shared.types import OpenCVImage
 
 class IContextualImageProcessor(ABC):
     @abstractmethod
-    def process(self, image: OpenCVImage) -> OpenCVImage:
+    def process(self, image: OpenCVImage, document_type: str) -> OpenCVImage:
         pass
--- src/modules/image_processing/use_cases/fix_image_orientation_use_case.py
+++ src/modules/image_processing/use_cases/fix_image_orientation_use_case.py
@@ -6,6 +6,7 @@ from ..interfaces import IContextualImageProcessor
 
 class Params(TypedDict):
     image_source: str
+    document_type: str
 
 
 class Result(TypedDict):
@@ -19,7 +20,10 @@ class FixImageOrientationUseCase(IUseCase[Params, Result]):
 
     def execute(self, params: Params) -> Result:
         image_source = params['image_source']
+        document_type = params['document_type']
         image = self.file_adapter.get_image(image_source)
-        processed_image = self.contextual_image_processor.process(image)
+        processed_image = self.contextual_image_processor.process(
+            image, document_type
+        )
         image_destination = self.file_adapter.save_image(processed_image)
         return {'image_destination': image_destination}
