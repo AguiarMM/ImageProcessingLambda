c46ee755d30e5a213f7d8c6de834f6d53794960e
Mateus Aguiar <aguiar_m@ymail.com>
2024-02-14 09:07:54 -0300

    feat: advancing text_orientation_strategy implementation

--- src/modules/image_processing/strategies/images/text_orientation_strategy.py
+++ src/modules/image_processing/strategies/images/text_orientation_strategy.py
@@ -1,7 +1,17 @@
-from shared.types import OpenCVImage
-from ...interfaces import IImageProcessingStrategy
+import pytesseract
+import cv2
+import imutils
+
+from src.shared.types import OpenCVImage
+from src.modules.image_processing.interfaces import IImageProcessingStrategy
 
 
 class TextOrientationStrategy(IImageProcessingStrategy):
     def execute(self, image: OpenCVImage) -> OpenCVImage:
-        pass
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pytesseract.image_to_osd(
+            rgb, output_type=pytesseract.Output.DICT)
+        assert isinstance(
+            results, dict), f"Expected a dictionary, got {type(results)}"
+        rotated = imutils.rotate_bound(image, angle=results['rotate'])
+        return rotated
